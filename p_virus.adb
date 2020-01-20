9415345bd2a197f8ab0530f5b944352bb387544c
ThomasDuple <thomasduple@gmail.com>
2020-01-20 09:16:51 +0100

    ajout procédures

--- p_virus.adb
+++ p_virus.adb
@@ -11,4 +11,49 @@ package body p_virus is
             end loop;
         end loop;
     end InitPartie;
+
+    procedure Configurer(f : in out p_piece_io.file_type; nb : in positive; Grille: in out TV_Grille; Pieces: in out TV_Pieces) is
+    --  {f ouvert, nb est un numéro de configuration (appelé numéro de partie),
+    --  une configuration décrit le placement des pièces du jeu, pour chaque configuration:
+    --      * les éléments d’une même pièce (même couleur) sont stockés consécutivement 
+    --      * il n’y a pas deux pièces mobiles ayant la même couleur 
+    --      *les deux éléments constituant le virus (couleur rouge)terminent la configuration}
+    --  =>  {Grillea été mis à jour par lecture dans f de la configuration de numéro nb
+    --          Pieces a été initialisé en fonction des pièces de cette configuration}
+    begin
+        -- à compléter
+    end Configurer;
+
+    --pour test configuration... 
+    procedure PosPiece(Grille: in TV_Grille; coul: in T_coulP) is
+    --{} => {la position de la pièce de couleur coul a été affichée si cette pièce est dans Grille
+    --       exemple: ROUGE: F4 G5}
+    begin
+        -- à compléter
+    end PosPiece;
+    
+    ---------------Contrôledu jeu
+    function Possible (Grille: in TV_Grille; coul: T_CoulP; Dir : in T_Direction) return boolean is
+    --  {coul/= blanc} 
+    --=> {résultat= vrai si la pièce de couleur coul peut être déplacée dans la direction Dir}
+    begin
+        -- à compléter
+    end Possible;
+
+
+    procedure MajGrille (Grille: in out TV_Grille; coul: in T_coulP; Dir :in T_Direction) is
+    --  {la pièce de couleur coul peut être déplacéedans la direction Dir} 
+    --=> {Grillea été mis à jour suite au déplacement}
+    begin
+        -- à compléter
+    end MajGrille;
+
+    function Guerison(Grille: in TV_Grille) return boolean is
+    --{} => {résultat = le virus (pièce rouge) est prêt à sortir (position coin haut gauche)}
+    begin
+        -- à compléter
+
+        return false;
+    end Guerison;
+
 end p_virus;
--- p_virus.ads
+++ p_virus.ads
@@ -2,7 +2,7 @@ with sequential_io; with p_esiut; use p_esiut;
 
 package p_virus is
 
-    ---------------Types pour représenterlagrille de jeu
+    ---------------Types pour représenter lagrille de jeu
 
     subtype T_Col is character range 'A'..'G';
     subtype T_Lig is integer range 1..7;
@@ -23,10 +23,10 @@ package p_virus is
     
     type TV_Pieces is array(T_coulP)of boolean;
   
-  ----Instanciation de sequential_iopour un fichier de TR_ElemP;
+  ----Instanciation de sequential_io pour un fichier de TR_ElemP;
     package p_piece_io is new sequential_io(TR_ElemP);  use p_piece_io;
   
-  ----type pour la direction dedéplacementdes pièces
+  ----type pour la direction de déplacementdes pièces
     type T_Direction is (bg, hg, bd, hd);
     package p_dir_io is new p_enum(T_Direction); use p_dir_io;
     
@@ -37,17 +37,17 @@ package p_virus is
     --      y compris les cases inutilisables
     --      Tous les éléments de Pieces ont été initialisés à false}
     procedure Configurer(f : in out p_piece_io.file_type; nb : in positive; Grille:in out TV_Grille; Pieces: in out TV_Pieces);
-    --  {f ouvert,nb est un numéro de configuration (appelé numéro de partie),
-    --  une configurationdécrit le placement des pièces du jeu, pour chaqueconfiguration:
-    --      * les éléments d’une même pièce(même couleur) sont stockés consécutivement 
+    --  {f ouvert, nb est un numéro de configuration (appelé numéro de partie),
+    --  une configuration décrit le placement des pièces du jeu, pour chaque configuration:
+    --      * les éléments d’une même pièce (même couleur) sont stockés consécutivement 
     --      * il n’y a pas deux pièces mobiles ayant la même couleur 
-    --      *les deux éléments constituant levirus (couleur rouge)terminent la configuration}
-    --  =>  {Grillea été mis à jour par lecturedans f dela configurationde numéronb
-    --          Pieces a été initialisé en fonction despièces de cette configuration}
+    --      *les deux éléments constituant le virus (couleur rouge)terminent la configuration}
+    --  =>  {Grillea été mis à jour par lecture dans f de la configuration de numéro nb
+    --          Pieces a été initialisé en fonction des pièces de cette configuration}
 
     --pour test configuration... 
     procedure PosPiece(Grille: in TV_Grille; coul: in T_coulP);
-    --{} => {la position de la pièce de couleur coul a été affichéesi cette pièce est dans Grille
+    --{} => {la position de la pièce de couleur coul a été affichée si cette pièce est dans Grille
     --       exemple: ROUGE: F4 G5}
     
     ---------------Contrôledu jeu
@@ -56,10 +56,10 @@ package p_virus is
     --=> {résultat= vrai si la pièce de couleur coul peut être déplacée dans la direction Dir}
     
     procedure MajGrille (Grille: in out TV_Grille; coul: in T_coulP; Dir :in T_Direction);
-    --  {la pièce de couleur coul peutêtre déplacéedans la direction Dir} 
-    --=> {Grillea été misàjour suite au déplacement}
+    --  {la pièce de couleur coul peut être déplacéedans la direction Dir} 
+    --=> {Grillea été mis à jour suite au déplacement}
 
     function Guerison(Grille: in TV_Grille) return boolean;
-    --{} => {résultat = le virus (pièce rouge) est prêtà sortir (position coin haut gauche)}
+    --{} => {résultat = le virus (pièce rouge) est prêt à sortir (position coin haut gauche)}
     
 end p_virus;
