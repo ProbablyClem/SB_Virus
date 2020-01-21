2597e39d1d7bc8f2b6fe101cffbae089d4706879
ThomasDuple <thomasduple@gmail.com>
2020-01-21 10:52:02 +0100

    grille

--- av_graph.adb
+++ av_graph.adb
@@ -4,8 +4,6 @@ procedure av_graph is
     f : p_piece_io.file_type;
     fGrille, fMenu : TR_Fenetre;
     grille : TV_Grille;
-    pieces : TV_Pieces;
-    numConfig : natural;
 begin
 
     p_piece_io.open(f, p_piece_io.in_file, "Parties");
