1babb066dc2d2216b69a6b938ddd46f30da2e284
Clement Guiton <clemguigui7@gmail.com>
2020-01-21 10:17:04 +0100

    desactive els cases vide

--- p_vuegraph.adb
+++ p_vuegraph.adb
@@ -1,6 +1,6 @@
 package body p_vuegraph is
 
-    procedure AffichefGrille(f : in out TR_Fenetre; grille : in TV_Grille) is
+    procedure AffichefGrille(f : in out TR_Fenetre; grille : in TV_Grille is
         largeur, hauteur : natural;
     begin
         largeur := 600;
@@ -15,5 +15,16 @@ package body p_vuegraph is
             null;
         end loop;
     end AffichefGrille;
+
+    procedure RefreshfGrille(f : in out TR_Fenetre; grille : TV_Grille) is
+
+    begin
+    for i in grille'range(1) loop
+        for y in grille'range(2) loop
+            if grille(i,y) = vide then
+                ChangerEtatBouton(f, t_lig'image(i) & t_col'image(y), arret);
+        end loop;
+    end loop;
+    end RefreshfGrille;
 
 end p_vuegraph;
